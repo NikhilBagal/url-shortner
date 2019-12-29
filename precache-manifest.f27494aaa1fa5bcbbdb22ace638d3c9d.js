e35479b7c6815640d7cc00ef335df7af8ab9c796
nikhil bagal <nbagal02@gmail.com>
2019-12-30 02:57:51 +0530

    Updates

---
+++ precache-manifest.f27494aaa1fa5bcbbdb22ace638d3c9d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e61112fd750c421634b87fafb5639bd",
+    "url": "/url-shortner/index.html"
+  },
+  {
+    "revision": "69ece0b21c591a8f38f3",
+    "url": "/url-shortner/static/css/main.21671f11.chunk.css"
+  },
+  {
+    "revision": "cf93d226e4c925d245aa",
+    "url": "/url-shortner/static/js/2.67106c8b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/url-shortner/static/js/2.67106c8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69ece0b21c591a8f38f3",
+    "url": "/url-shortner/static/js/main.86915bba.chunk.js"
+  },
+  {
+    "revision": "e9d1a1b0afb46f7e8fe8",
+    "url": "/url-shortner/static/js/runtime-main.f2294f20.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/url-shortner/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/url-shortner/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/url-shortner/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "7febcff9f143b83553d544fc04fc5b09",
+    "url": "/url-shortner/static/media/icon-facebook.7febcff9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/url-shortner/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "996b697c33ab8772e59e3c9bec1124ea",
+    "url": "/url-shortner/static/media/icon-instagram.996b697c.svg"
+  },
+  {
+    "revision": "c557c3d4e80b5eda1ae120c8c2257e26",
+    "url": "/url-shortner/static/media/icon-pinterest.c557c3d4.svg"
+  },
+  {
+    "revision": "cb4fea0b4771a5dfc70f787d5e01c91e",
+    "url": "/url-shortner/static/media/icon-twitter.cb4fea0b.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/url-shortner/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/url-shortner/static/media/logo.6387840f.svg"
+  }
+]);
